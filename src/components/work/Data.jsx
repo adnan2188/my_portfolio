f8d317c417a2072d8ecdbc78b8450bee72ebd188
adnan2188 <madnan2188@gmail.com>
2024-03-04 16:45:48 +0500

    some projects added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,56 +1,73 @@
-import clock from '../../assets/clock.png';
-import calculator from '../../assets/calculator.png';
-import tick_tack_toe_game from '../../assets/tick-tack-toe-game.png';
-import todo from '../../assets/todo.png';
-import Work5 from '../../assets/work5.jpg';
+import clock from "../../assets/clock.png";
+import calculator from "../../assets/calculator.png";
+import tick_tack_toe_game from "../../assets/tick-tack-toe-game.png";
+import todo from "../../assets/todo.png";
+import Work5 from "../../assets/work5.jpg";
+import Work6 from "../../assets/work6.jpg";
+import Work7 from "../../assets/work7.jpg";
 export const projectsData = [
-    {
-        id: 1,
-        image: clock,
-        title: "Web design",
-        category: "web",
-        link: "https://adnan2188.github.io/Analog-Clock3/"
-    },
-    {
-        id: 2,
-        image: calculator,
-        title: "Calculator",
-        category: "app",
-        link: "https://yoursmartcalculator.netlify.app"
-    },
-    {
-        id: 3,
-        image: tick_tack_toe_game,
-        title: "Tick Tack Toe Game",
-        category: "design",
-        link: "https://myticktacktoegame.netlify.app"
-    },
-    {
-        id: 4,
-        image: todo,
-        title: "Todo Task",
-        category: "app",
-        link: "https://adnan2188.github.io/Todo_Task"
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: "Web design",
-        category: "web",
-    },
+  {
+    id: 1,
+    image: clock,
+    title: "Wall watch",
+    category: "web",
+    link: "https://adnan2188.github.io/Analog-Clock3/",
+  },
+  {
+    id: 2,
+    image: calculator,
+    title: "Calculator",
+    category: "app",
+    link: "https://your-smart-calculator.netlify.app",
+  },
+  {
+    id: 3,
+    image: tick_tack_toe_game,
+    title: "Tick Tack Toe Game",
+    category: "App",
+    link: "https://adnan2188.github.io/tic-tac-toe-game/",
+  },
+  {
+    id: 4,
+    image: todo,
+    title: "Todo Task",
+    category: "app",
+    link: "https://adnan2188.github.io/Todo_Task",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Qurna - Surahs",
+    category: "web",
+    link: "https://quran-surahs.netlify.app/",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Qurna Tranalation",
+    category: "web",
+    link: "https://quran-translate.netlify.app",
+  },
+  {
+    id: 7,
+    image: Work7,
+    title: "College Website",
+    category: "web",
+    link: "https://adnan2188.github.io/CollegeWebsite/",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: "all",
-    },
-    {
-        name: "web",
-    },
-    {
-        name: "app",
-    },
-    {
-        name: "design",
-    },
-]
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "design",
+  },
+];
